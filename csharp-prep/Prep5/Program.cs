c9d029e5a85876f052141253685852d0e3f5949e
tebtione <97209453+tebtione@users.noreply.github.com>
2024-11-09 14:39:51 -0700

    Completed Prep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,22 @@
 using System;
 
-class Program
+namespace Prep5
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello Prep5 World!");
+
+            Console.WriteLine("Welcome to the Program!");
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Please entery your favourite number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int squared = number * number;
+            Console.WriteLine($"{name}, the square of your number is {squared}");
+        }
     }
 }
