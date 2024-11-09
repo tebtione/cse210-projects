5a3989454c9d6004e19e797d108ad4f71cecf7c5
tebtione <97209453+tebtione@users.noreply.github.com>
2024-11-09 07:00:22 -0700

    Completed Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.WriteLine("Hello Prep3 World!");
+
+        Random random = new Random();
+        int magicNumber = random.Next(1, 100);
+
+        int guessNumber = 0;
+        while (guessNumber != magicNumber)
+        {
+            Console.Write("What is your guess number? ");
+            guessNumber = int.Parse(Console.ReadLine());
+
+            if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
