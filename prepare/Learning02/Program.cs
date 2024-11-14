4b537851b3ca26c452ed9053684cdf8485e0b163
tebtione <97209453+tebtione@users.noreply.github.com>
2024-11-14 03:46:15 -0700

    Make changes to Program assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        //Console.WriteLine("Hello Learning02 World!");
+    
+        //Create Job instances
+        Job job1 = new Job("Softwar Engineer", "Micrsoft", 2019, 2022);
+        Job job2 = new Job("Manager", "Apple", 2022, 2023);
+
+        //Create Resume instance
+        Resume myResume = new Resume("Allision Rose");
+
+        myResume.AddJob(job1);
+        myResume.AddJob(job2);
+
+        //Display the resume
+        myResume.Display();
     }
 }
