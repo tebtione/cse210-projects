0f37b7f37485c5a729d8a04423b940f612284576
tebtione <97209453+tebtione@users.noreply.github.com>
2024-11-23 12:45:16 -0700

    ScriptureLibrary still working ...

---
+++ prove/Develop03/ScriptureLiabrary.cs
@@ -0,0 +1,14 @@
+using System;
+
+//public class ScriptureLibrary
+//{
+//private List<Scripture> _scriptures = new List<Scripture>();
+//public void LoadFromFile(string filepath)
+// {
+
+//}
+//public Scripture GetRandomScripture()
+//{
+
+// }
+//}
