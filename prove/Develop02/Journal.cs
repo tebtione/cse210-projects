ab5bf242c23f23fd53206b157bdeee17620e43d2
tebtione <97209453+tebtione@users.noreply.github.com>
2024-11-25 21:36:35 -0700

    Make changes using underscore variables

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -3,27 +3,27 @@ using System.IO;
 
 public class Journal
 {
-    public List<Entry> Entries { get; private set; } = new List<Entry>(); // List of journal entries
-    public void AddEntry(Entry entry) => Entries.Add(entry); // Adds a new entry to the journal
+    public List<Entry> _entries { get; private set; } = new List<Entry>(); // List of journal entries
+    public void AddEntry(Entry _entry) => _entries.Add(_entry); // Adds a new entry to the journal
     public void DisplayEntries() // Displays all journal entries
     {
-        foreach (var entry in Entries)
-            Console.WriteLine($"{entry.Date}: {entry.Prompt} - {entry.Response}");
+        foreach (var _entry in _entries)
+            Console.WriteLine($"{_entry.Date}: {_entry.Prompt} - {_entry.Response}");
     }
-    public void SaveToFile(string filename) // Saves entries to a specified file
+    public void SaveToFile(string _filename) // Saves entries to a specified file
     {
-        using var writer = new StreamWriter(filename);
-        foreach (var entry in Entries)
-            writer.WriteLine(entry.FormatEntry());
+        using var writer = new StreamWriter(_filename);
+        foreach (var _entry in _entries)
+            writer.WriteLine(_entry.FormatEntry());
     }
-    public void LoadFromFile(string filename) // Loads entries from a specified file
+    public void LoadFromFile(string _filename) // Loads entries from a specified file
     {
-        Entries.Clear();
-        foreach (var line in File.ReadAllLines(filename))
+        _entries.Clear();
+        foreach (var _line in File.ReadAllLines(_filename))
         {
-            var parts = line.Split('|');
-            if (parts.Length == 3)
-                Entries.Add(new Entry { Date = parts[0], Prompt = parts[1], Response = parts[2] });
+            var _parts = _line.Split('|');
+            if (_parts.Length == 3)
+                _entries.Add(new _entry { Date = _parts[0], Prompt = _parts[1], Response = _parts[2] });
         }
     }
 }
