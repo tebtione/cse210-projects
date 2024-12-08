4a73553e47f24b901b3ecb40e360c1f504f2f98a
tebtione <97209453+tebtione@users.noreply.github.com>
2024-12-07 22:01:58 -0700

    Program completed

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        // Console.WriteLine("Hello Develop05 World!");
+
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Choose an activity:");
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Reflection Activity");
+            Console.WriteLine("3. Listing Activity");
+            Console.WriteLine("4. Exit");
+
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                BreathingActivity breathingActivity = new BreathingActivity();
+
+                breathingActivity.StartActivity();
+                breathingActivity.ExecuteActivity();
+                break;
+
+                case "2":
+                ReflectionActivity reflectionActivity = new ReflectionActivity();
+
+                reflectionActivity.StartActivity();
+                reflectionActivity.ExecuteActivity();
+                break;
+
+                case "3":
+                ListingActivity listActivity = new ListingActivity();
+
+                listActivity.StartActivity();
+                listActivity.ExecuteActivity();
+                break;
+
+                case "4":
+                return;
+
+                default:
+                Console.WriteLine("Invalid choice. Please try again.");
+                break;
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            
+        }
     }
 }
