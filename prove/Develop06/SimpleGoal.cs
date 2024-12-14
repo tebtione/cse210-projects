e935887c63506a2149a4df4d6bc58f0b6b3cdaf8
tebtione <97209453+tebtione@users.noreply.github.com>
2024-12-14 16:47:45 -0700

    Completed and updated SimpleGoal program

--- prove/Develop06/SimpleGoal.cs
+++ prove/Develop06/SimpleGoal.cs
@@ -4,16 +4,19 @@ namespace EternalQuest
 {
     public class SimpleGoal : Goal
     {
+        // Constructor initializing the simple goal
         public SimpleGoal(string name, int points) : base(name, points)
         {
 
         }
 
+        // Marks the goal as completed
         public override void RecordEvent()
         {
             IsCompleted = true;
         }
 
+        // Returns the status of the simple goal (completed or not)
         public override string GetStatus()
         {
             return IsCompleted ? "[X] " + Name : "[ ]" + Name;
