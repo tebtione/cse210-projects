6b883f6128932c11ea4f48e28fbaedd58930d110
tebtione <97209453+tebtione@users.noreply.github.com>
2024-12-14 08:35:05 -0700

    Updated corrections and completed

--- prove/Develop05/BreathingActivity.cs
+++ prove/Develop05/BreathingActivity.cs
@@ -1,21 +1,26 @@
 using System;
 
 namespace MindfulnessProgram
-{   
+{
     // Class for the Breathing Activity
-    public class BreathingActivity : MindfulnessProgram
+    public class BreathingActivity : MindfulnessActivity
     {
-        public void ExecuteActivity()
-        {   
+        public BreathingActivity() : base("Breathing Activity", "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.")
+        {
+
+        }
+        public override void Execute()
+        {
+            StartActivity();
             // Set end time based on duration
             DateTime endTime = DateTime.Now.AddSeconds(_durationInSeconds);
 
             while (DateTime.Now < endTime)
             {
                 Console.WriteLine("Breathe in...");
-                ShowCountdown(4);
+                ShowSpinner(4);
                 Console.WriteLine("Breathe out...");
-                ShowCountdown(4);
+                ShowSpinner(4);
             }
             EndActivity(); // Call to end the activity
         }
