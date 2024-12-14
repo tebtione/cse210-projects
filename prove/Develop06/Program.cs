c594225946fe6dd54cb836fff19865c82da62978
tebtione <97209453+tebtione@users.noreply.github.com>
2024-12-14 13:30:45 -0700

    Completed and Updated Program

--- prove/Develop06/Program.cs
+++ prove/Develop06/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop06 World!");
+        // Console.WriteLine("Hello Develop06 World!");
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 4, 6));
+        shapes.Add(new Circle("Green", 3));
+
+        foreach (Shape shape in shapes)
+        {
+            shape.DisplayInfo();
+            Console.WriteLine($"Area: {shape.GetArea()}");
+        }
     }
 }
