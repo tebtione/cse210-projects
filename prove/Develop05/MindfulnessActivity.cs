0a0b5323f51e8b0ab02769e1cf2732974a5f08e3
tebtione <97209453+tebtione@users.noreply.github.com>
2024-12-14 08:36:37 -0700

    Updated code errors and completed

--- prove/Develop05/MindfulnessActivity.cs
+++ prove/Develop05/MindfulnessActivity.cs
@@ -4,44 +4,48 @@ using System.Threading;
 namespace MindfulnessProgram
 {
     // Base class for mindfulness activities
-    public class MindfulnessProgram
+    public abstract class MindfulnessActivity
     {
         protected int _durationInSeconds; // Duration of the activity in seconds
         protected string _activityName;
         protected string _description;
 
-        // Starts the activity by displaying its name and description
-        public void StartActivity(string activityName, string description)
+        public MindfulnessActivity(string activityName, string description)
         {
             _activityName = activityName;
             _description = description;
-
+        }
+        // Starts the activity by displaying its name and description
+        public void StartActivity()
+        {
             Console.Clear();
-            Console.WriteLine($"--- {_activityName} ---");
+            Console.WriteLine($"Starting: --- {_activityName} ---\n");
             Console.WriteLine(_description);
             Console.WriteLine("Enter duration in seconds: ");
             _durationInSeconds = int.Parse(Console.ReadLine());
             Console.WriteLine("Get ready...");
-            ShowCountdown(5);
+            ShowSpinner(5);
         }
-        
-        // Display a countdown
-        public void ShowCountdown(int seconds)
+
+        // End the activity and gives feedback to the user
+        protected void EndActivity()
         {
-            for (int i = seconds; i > 0; i==)
+            Console.WriteLine("Well done!");
+            Console.WriteLine($"You completed the {_activityName} for {_durationInSeconds} seconds");
+            ShowSpinner(5);
+        }
+
+        // Display a countdown
+        public void ShowSpinner(int seconds)
+        {
+            for (int i = 0; i < seconds; i++)
             {
-                Console.Write(i + " ");
+                Console.Write(".");
                 Thread.Sleep(1000);
             }
             Console.WriteLine();
         }
-        
-        // End the activity and gives feedback to the user
-        protected void EndActivity()
-        {
-           Console.WriteLine("Well done!");
-           Console.WriteLine($"You completed the (_activityName) for {_durationInSeconds} seconds");
-           ShowCountdown(5) 
-        }
+
+        public abstract void Execute();
     }
 }
