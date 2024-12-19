7baff80aed647e5d774ae26dd669eb8f21d99703
tebtione <97209453+tebtione@users.noreply.github.com>
2024-12-18 19:03:10 -0700

    Completed and updated the main program

--- foundation/Foundation3/Program.cs
+++ foundation/Foundation3/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        // Console.WriteLine("Hello Foundation3 World!");
+
+        List<Activity> activities = new List<Activity>
+        {
+            new Running(30, 5),
+            new Cycling(45, 15),
+            new Swimming(20, 30),
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
