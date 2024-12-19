0b74c64a8c37a1d0d9dabc612a982fa32c4a0425
tebtione <97209453+tebtione@users.noreply.github.com>
2024-12-18 19:26:16 -0700

    Completed and updated cycling

--- foundation/Foundation3/Cycling.cs
+++ foundation/Foundation3/Cycling.cs
@@ -2,12 +2,17 @@ using System;
 
 public class Cycling : Activity
 {
+    // Private member variables to store the distance
     private double _distanceKm;
+
+    // Constructor to initialize the cyclying activity
     public Cycling(int minutes, double distanceKm) : base(minutes)
     {
         _distanceKm = distanceKm;
     }
 
+    // Override the GetDistance method
+    // to return the distance of the ride
     public override double GetDistance()
     {
         return _distanceKm;
