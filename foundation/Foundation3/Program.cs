c9013116d58c16c68dadcd04280450f746357395
tebtione <97209453+tebtione@users.noreply.github.com>
2024-12-18 19:26:36 -0700

    Complete and updated main program

--- foundation/Foundation3/Program.cs
+++ foundation/Foundation3/Program.cs
@@ -7,13 +7,15 @@ class Program
     {
         // Console.WriteLine("Hello Foundation3 World!");
 
+        // Create a list to store different types of activities
         List<Activity> activities = new List<Activity>
         {
-            new Running(30, 5),
-            new Cycling(45, 15),
-            new Swimming(20, 30),
+            new Running(30, 5), // Create a running activity
+            new Cycling(45, 15), // Create a cycling activity
+            new Swimming(20, 30), // Create a swimming activity
         };
 
+        // Iterate through the list of activities and print
         foreach (var activity in activities)
         {
             Console.WriteLine(activity.GetSummary());
