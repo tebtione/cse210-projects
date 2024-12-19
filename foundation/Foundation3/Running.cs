a02fce070a839f80d77a79f025f11035425fdfb4
tebtione <97209453+tebtione@users.noreply.github.com>
2024-12-18 19:27:00 -0700

    Completed and updated running activity

--- foundation/Foundation3/Running.cs
+++ foundation/Foundation3/Running.cs
@@ -1,13 +1,19 @@
 using System;
 
+// Class representing a running activity, 
+// derived from the base Activity class
 public class Running : Activity
 {
-    private double _distanceKm;
+    private double _distanceKm; // Private member variable
+
+    // Constructor to initialize the running activity
     public Running(int minutes, double distanceKm) : base(minutes)
     {
         _distanceKm = distanceKm;
     }
 
+    // Override the GetDistance method
+    // to return the distance of the run
     public override double GetDistance()
     {
         return _distanceKm;
