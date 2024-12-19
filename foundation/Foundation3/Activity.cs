5afc0910d157d6b969611eb73e1e36f588fb9587
tebtione <97209453+tebtione@users.noreply.github.com>
2024-12-18 19:25:56 -0700

    Completed and updated Activity

--- foundation/Foundation3/Activity.cs
+++ foundation/Foundation3/Activity.cs
@@ -1,26 +1,29 @@
 using System;
 
-public abstract class Activity
+public abstract class Activity // Base class
 {
-    protected int _minutes;
+    protected int _minutes; // Protected membner variable to store the duration
+
+    // Constructor to initialize the activity
     public Activity(int minutes)
     {
         _minutes = minutes;
     }
 
+    // Abstract method to get the distance
     public abstract double GetDistance();
-    public double GetSpeed()
+    public double GetSpeed() // Method to calculate speed
     {
         double distance = GetDistance();
         return distance > 0 ? (distance / _minutes) * 60 : 0;
     }
-    public double GetPace()
+    public double GetPace() // Method to calculate pace
     {
         double distance = GetDistance();
         return distance > 0 ? _minutes / distance : 0;
     }
 
-    public virtual string GetSummary()
+    public virtual string GetSummary() // Method to get a summary of the activity details
     {
         double distance = GetDistance();
         double speed = GetSpeed();
