74ceb20daa057ace73b43be468d26d6fbf3823dc
tebtione <97209453+tebtione@users.noreply.github.com>
2024-12-18 19:27:20 -0700

    Completed and updated swimming program

--- foundation/Foundation3/Swimming.cs
+++ foundation/Foundation3/Swimming.cs
@@ -1,8 +1,13 @@
 using System;
 
+// Class representing a swimmimg activity
+// derive from the base activity class
 public class Swimming : Activity
 {
+    // Private member variable to store the number of laps swum
     private int _laps;
+
+    // Constructor to initialize the swimming activity
     public Swimming(int minutes, int laps) : base(minutes)
     {
         _laps = laps;
@@ -10,6 +15,6 @@ public class Swimming : Activity
 
     public override double GetDistance()
     {
-        return (_laps * 50 / 1000.0);
+        return (_laps * 50 / 1000.0); // Calculate distance
     }
 }
